208f6dd9a11f77e147290bf26ff802ac34138470
Utkarsh450 <ubarnwal0802@gmail.com>
2025-12-22 14:57:10 +0530

    done2

--- Notes-Frontend/src/pages/Barchart.tsx
+++ Notes-Frontend/src/pages/Barchart.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'
+import { useContext } from 'react'
 import MonthlyAnalytics from '../components/Bar'
 import { ExpenseContextData } from '../Context/ExpenseContext'
 
