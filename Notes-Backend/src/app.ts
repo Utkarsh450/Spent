4c66688c386463d7620ae43f5f460f717259a8de
Utkarsh450 <ubarnwal0802@gmail.com>
2026-01-09 16:54:13 +0530

    Add frontend files and service worker for PWA support
    - Created index.html for the frontend entry point.
    - Added manifest.webmanifest for PWA configuration.
    - Included service worker registration script (registerSW.js).
    - Implemented service worker (sw.js) for caching and offline capabilities.
    - Added icons and assets for the PWA.
    - Updated app.ts to serve static files and handle wildcard routes.
    - Modified axiosConfig.tsx to point to the new API base URL.

--- Notes-Backend/src/app.ts
+++ Notes-Backend/src/app.ts
@@ -3,10 +3,13 @@ import authRoutes from "./routes/user.routes"
 import productRoutes from "./routes/money.routes";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import path from "path"
 
 const app: Application = express()
 
 app.use(express.json())
+
+app.use(express.static( path.join(__dirname, "../public")))
 
 app.use(cors({
     // origin: "https://spent-yr8d.vercel.app",
@@ -19,5 +22,8 @@ app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes)
+app.get("*name", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/html"))
+})
 
 export default app
