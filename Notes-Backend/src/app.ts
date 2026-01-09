0b546bb10072a6f78fff21a6b3da343efd7965b7
Utkarsh450 <ubarnwal0802@gmail.com>
2026-01-09 14:35:56 +0530

    deploy done

--- Notes-Backend/src/app.ts
+++ Notes-Backend/src/app.ts
@@ -9,7 +9,7 @@ const app: Application = express()
 app.use(express.json())
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://spent-yr8d.vercel.app",
     credentials: true,
 }));
 app.use(cookieParser());
